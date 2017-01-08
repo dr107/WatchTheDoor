feaeedddc3f0db3a257f231a6c1fd7b5be85ebcb
Dan <dan.rohr107@gmail.com>
2017-01-08 17:08:29 +0000

    Add example supervisor conf file

--- WatchTheDoor/run.py
+++ WatchTheDoor/run.py
@@ -6,6 +6,12 @@ from flask_injector import FlaskInjector
 from app import app
 from app.dep_inject import WatchTheDoorModule
 from app.face_detector import Detector
+from os.path import abspath, dirname
+from os import chdir
+
+abs_path = abspath(__file__)
+dir = dirname(abs_path)
+chdir(dir)
 
 logging.basicConfig()
 logging.root.setLevel(logging.WARN)
